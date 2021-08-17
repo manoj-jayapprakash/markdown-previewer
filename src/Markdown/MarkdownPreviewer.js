48c992b9897e17d35a7aac8fa660b9c061d7065a
Manoj Jayapprakash <manoj.jayapprakash@gmail.com>
2021-08-17 11:32:46 +0530

    Added markdown to html conversion logic

--- src/Markdown/MarkdownPreviewer.js
+++ src/Markdown/MarkdownPreviewer.js
@@ -1,13 +1,21 @@
 import './MarkdownPreviewer.css';
+import marked from 'marked';
 
 export const MarkdownPreviewer = () => {
+  const markdownConverter = (str) => {
+    return { __html: marked(str) };
+  };
+
   return (
     <section className="previewer">
       <div className="section__header">
         <h2>Previewer</h2>
       </div>
       <div className="section__body">
-        <div id="preview"></div>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={markdownConverter('# Markdown')}
+        />
       </div>
     </section>
   );
